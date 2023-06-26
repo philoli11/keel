bd4391bfa9a6538383c6f7bffceb79948a46ecdb
philoli11 <1099227528@qq.com>
2023-06-26 19:53:20 +0800

    修改注释

--- keel-core/src/main/java/io/philo/framework/keel/context/NamedFunction.java
+++ keel-core/src/main/java/io/philo/framework/keel/context/NamedFunction.java
@@ -5,13 +5,10 @@ import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.Method;
 import java.util.function.Function;
 
-//函数式接口注解
 @FunctionalInterface
 public interface NamedFunction<T, R> extends Function<T, R>, Serializable {
 
-    //这个方法返回的SerializedLambda是重点
     default SerializedLambda getSerializedLambda() throws Exception {
-        //writeReplace改了好像会报异常
         Method write = this.getClass().getDeclaredMethod("writeReplace");
         write.setAccessible(true);
         return (SerializedLambda) write.invoke(this);
